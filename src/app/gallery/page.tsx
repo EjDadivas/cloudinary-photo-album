583b103eec0b2c727d3f8c9c0af8b22330fc6daf
ej <99721216+EjDadivas@users.noreply.github.com>
2023-10-11 20:26:47 +0800

    show images on gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,23 +1,40 @@
-"use client"
-import { CldUploadButton } from "next-cloudinary";
-import { UploadResult } from "../page";
-import { Button } from "@/components/ui/button";
+import { CloudinaryImage } from "./cloudinary-image";
+import UploadButton from "./upload-button";
+import cloudinary from "cloudinary"
 
-export default function GalleryPage(){
-    return <section>
+type SearchResult = {
+    public_id: string
+}
+export default async function GalleryPage(){
+   const results = (await cloudinary.v2.search
+    .expression('resource_type:image')
+    .sort_by('created_at','desc')
+    .max_results(50)
+    .execute()) as {resources:  SearchResult[]}
+
+
+    return (
+    <section>
+        <div className="flex flex-col gap-8">
         <div className="flex justify-between">
-        <h1 className="text-4xl font-bold">Gallery</h1>
-        <Button asChild>
-        <div className="flex- gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-             <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-        </svg>
-        <CldUploadButton uploadPreset="jzaofbjj" 
-        onUpload={(result: UploadResult)=> console.log(result.info.public_id)}
-        />
+            <h1 className="text-4xl font-bold">Gallery</h1>
+            <UploadButton
+            
+            />
         </div>
-    
-      </Button>
+        <div className="grid grid-cols-4 gap-4">
+        {results.resources.map(result=> (
+          <CloudinaryImage  
+          key={result.public_id}
+          width="400"
+          height="300"
+          src={result.public_id}
+          sizes="100vw"
+          alt="Description of my image"
+        />
+          ))}
+        </div>
         </div>
     </section>
+    )
 }
