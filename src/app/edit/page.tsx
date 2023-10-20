9b5aa82f8a89096d9514ec5db481639acdd5c987
ej <99721216+EjDadivas@users.noreply.github.com>
2023-10-20 14:32:19 +0800

    generative fill with prompts

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { CldImage } from "next-cloudinary";
 import { useState } from "react";
 
 export default function EditPage({searchParams: {publicId}}: {searchParams: {publicId:string}}){
     const [transformation, setTransformation] = useState<undefined | "generative-fill" | "blur" | "remove-background" | "grayscale" | "pixelate">()
+    const [prompt, setPrompt] = useState("")
+    const [pendingPrompt, setPendingPrompt] = useState("");
     return (
         <section>
 
@@ -16,7 +20,23 @@ export default function EditPage({searchParams: {publicId}}: {searchParams: {pub
 
        <div className="flex gap-4">
        <Button variant="ghost" onClick={()=>setTransformation(undefined)}>Clear All </Button>
-        <Button variant="default" onClick={()=>setTransformation("generative-fill")}>Generative Fill</Button>
+       
+       <div className="flex flex-col gap-4">
+            <Button
+              onClick={() => {
+                setTransformation("generative-fill");
+                setPrompt(pendingPrompt);
+              }}
+            >
+              Apply Generative Fill
+            </Button>
+            <Label>Prompt</Label>
+            <Input
+              value={pendingPrompt}
+              onChange={(e) => setPendingPrompt(e.currentTarget.value)}
+            />
+          </div>
+
         <Button variant="default" onClick={()=>setTransformation("blur")}>Blur</Button>
         <Button variant="default" onClick={()=>setTransformation("remove-background")}>Remove Background</Button>
         <Button variant="default" onClick={()=>setTransformation("grayscale")}>Grayscale</Button>
@@ -27,9 +47,13 @@ export default function EditPage({searchParams: {publicId}}: {searchParams: {pub
         <CldImage src={publicId} alt="some image" width="300" height="200"/>
 
         {transformation === 'generative-fill' &&
-        <CldImage src={publicId} alt="some image" width="1200" height="1400"
+        <CldImage src={publicId} alt="some image" width="1800" height="1400"
          crop="pad"
-         fillBackground
+         fillBackground={
+            {
+                prompt: prompt
+            }
+         }
          />
         }
          {transformation === 'blur' &&
